4257c0d0d2eccdd4eb612bbd3f93ca82332c4dd2
Vito Faraco Junior <vfajr84@gmail.com>
2026-05-19 10:49:20 -0300

    Finalizado a primeira versao

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyExtension.Tests;
+
+[TestClass]
+public sealed class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()
+    {
+        decimal valor = 279.98m;
+        var cents = valor.ToCents();
+
+        Assert.AreEqual(27998, cents);
+    }
+}
